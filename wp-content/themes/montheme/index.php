ce74ab8d6437e87fa71319b7fb9d6a618916671e
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-04-23 10:49:27 +0200

    WP les boucles

--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,7 +1,29 @@
 <!--Partie entête généré par WP-->
 <?php get_header() ?>
 
-    Bonjour tout le monde : <?php wp_title(); ?>
+<!--boucle WP | Début avec une condition-->
+<?php if (have_posts()) : ?>
+    <div class="row">
+        <!--parcours de la boucle-->
+        <?php while (have_posts()) : the_post(); ?>
+            <!--the_post déclare l'article-->
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text">
+                            <?php the_excerpt() ?>
+                        </p>
+                        <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile ?>
+    </div>
+<?php else : ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
 
 <!--Partie footer généré par WP-->
 <?php get_footer() ?>
