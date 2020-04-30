3e5ebf40d6b10c540075f38a798cfa39ccdf2a11
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-04-30 13:40:36 +0200

    api customize

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -1,5 +1,6 @@
 <?php 
 require_once('walker/CommentWalker.php');
+require_once('options/apparence.php');
 
 function montheme_supports () {
     add_theme_support('title-tag');
@@ -20,8 +21,10 @@ function montheme_register_assets () {
     wp_register_style('bootstrap',  'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', []);
     wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
     wp_register_script('popper',    'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', [], false, true);
-    wp_deregister_script('jquery');
-    wp_register_script('jquery',    'https://code.jquery.com/jquery-3.4.1.slim.min.js', [], false, true);
+    if(!is_customize_preview()) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery',    'https://code.jquery.com/jquery-3.4.1.slim.min.js', [], false, true);
+    }
     wp_enqueue_style('bootstrap');
     wp_enqueue_script('bootstrap');
 }
